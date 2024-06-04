f27aee23a46968c1a2fc15617deae7cd4867ea61
domen <domen.laznik@student.um.si>
2024-06-04 13:23:07 +0200

    Gathered all the data and split them from 80/20

--- learning.py
+++ learning.py
@@ -4,8 +4,21 @@ from torchvision import datasets, models, transforms
 import torch.nn as nn
 from torch.optim import Adam
 from torch.utils.data import random_split, DataLoader
+from torch.utils.data import ConcatDataset
 
-transforms = transforms.Compose([
+transform = transforms.Compose([
     transforms.toTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
+
+data_dir = 'learning'
+classes = ['domen', 'nejc', 'nik', 'none']
+datasets = {x: datasets.ImageFolder(os.path.join(data_dir, f'learning_{x}'), transform) for x in classes}
+
+all_datasets = ConcatDataset([datasets[x] for x in classes])
+
+total_size = len(all_datasets)
+train_size = int(0.8 * total_size)
+val_size = total_size - train_size
+
+train_dataset, val_dataset = random_split(all_datasets, [train_size, val_size])
